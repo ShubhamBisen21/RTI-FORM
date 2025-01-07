a6dac573e03689532164843ec06209309aebd790
Shubham Bisen <shubhambisen21@gmail.com>
2025-01-07 13:04:43 +0530

    RTI form Created

---
+++ src/app/mainbody/mainbody.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainbody',
+  imports: [],
+  templateUrl: './mainbody.component.html',
+  styleUrl: './mainbody.component.css'
+})
+export class MainbodyComponent {
+
+}
